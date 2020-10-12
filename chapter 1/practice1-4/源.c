4a15e9c62274d17294fc92c31994219134ec5170
e <2738422036@qq.com>
2020-10-12 23:49:01 +0800

    xiugai

--- chapter 1/practice1-4/源.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a = 3.14;
-	printf("%d", a);
-	return 0;
-	/*输出的是3,很容易出问题*/
-}
--- chapter 1/practice1-7/源.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	puts("天");
-	puts("地");
-	puts("人");
-	//无格式化输出，puts=printf+\n
-	return 0;
-}
